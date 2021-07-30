131bcf5be6906acb6f49dd11768af644538e8b4a
TangerineMoss9 <77404972+TangerineMoss9@users.noreply.github.com>
2021-07-30 15:18:05 -0400

    Adapter Pattern

---
+++ Adapter_Pattern/Adapter/src/mypackage/MallardDuck.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class MallardDuck implements Duck{
+	public void quack() {
+		System.out.println("Quack");
+	}
+	public void fly() {
+		System.out.println("I'm flying");
+	}
+}
